f3d928635d77d606473163bcd79bb816edaa214d
keenzman <szaman_03@hotmail.co.uk>
2019-10-22 01:00:43 +0100

    Add typed intro

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	var typed = new Typed(".typed", {
+			strings: ["Recent Graduate from King's College London.", "UX/UI Enthusiast.", "Front-End Developer."],
+			typeSpeed: 90,
+			loop: true,
+			startDelay: 1000,
+			showCursor: false
+
+		});
+});
